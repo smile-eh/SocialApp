e0ff112cc186701b220bd94a0688e67a79b40a75
Matthew C <30327544+smile-eh@users.noreply.github.com>
2023-04-26 13:55:24 -0400

    init commit

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = "Shit weather"
+        })
+        .ToArray();
+    }
+}
