9f8816ebc9f0f3bae5937756b8ea71c7a7f0c605
Matthew C <30327544+smile-eh@users.noreply.github.com>
2023-05-23 16:07:28 -0400

    rename project to social app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title: string = 'Dating App';
+  title: string = 'Social App';
   users: any;
 
   constructor(private http: HttpClient) { }
