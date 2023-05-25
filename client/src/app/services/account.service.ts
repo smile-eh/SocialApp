f8b66494fee21797336c5fc37a21b29a3173c4fe
Matthew C <30327544+smile-eh@users.noreply.github.com>
2023-05-25 13:05:25 -0400

    add login/logout to account service

--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { User } from '../models/user';
+import { Login } from '../models/login';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +10,25 @@ import { Injectable } from '@angular/core';
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  login(login: Login) {
+    return this.http
+      .post<User>(this.baseUrl + 'account/login', {
+        username: login.username,
+        password: login.password,
+      })
+      .pipe(
+        map((response: User) => {
+          const user = response;
+          if (user) {
+            localStorage.setItem('user', JSON.stringify(user));
+          }
+        })
+      );
+  }
+
+  lougout() {
+    localStorage.removeItem('user');
+  }
 }
