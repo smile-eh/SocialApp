59d2290700cb1c8506b04c2a1b5346ec7332f73c
Matthew C <30327544+smile-eh@users.noreply.github.com>
2023-05-25 15:05:19 -0400

    add initial register functions

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Register } from '../models/register';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  regModel: Register = { username: '', password: '' };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  register() {
+    console.log(this.regModel);
   }
 
+  cancel() {
+    console.log('cancel');
+  }
 }
