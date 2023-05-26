59fdf3ec2ec9fc022a4cf1d8619b015b13d51dbd
Matthew C <30327544+smile-eh@users.noreply.github.com>
2023-05-26 12:01:27 -0400

    display errors instead of logging to console

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import { Register } from '../models/register';
 import { AccountService } from '../services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,10 @@ export class RegisterComponent implements OnInit {
 
   regModel: Register = { username: '', password: '' };
 
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
@@ -21,7 +25,7 @@ export class RegisterComponent implements OnInit {
       next: () => {
         this.cancel();
       },
-      error: (err) => console.log(err),
+      error: (e) => this.toastr.error(e.error.title),
     });
   }
 
