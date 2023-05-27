deb774c63da770ff854c1ffcb3b11efb723285bf
Matthew C <30327544+smile-eh@users.noreply.github.com>
2023-05-27 19:18:57 -0400

    add page to test errors

---
+++ client/src/app/errors/test-error/test-error.component.ts
@@ -0,0 +1,71 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.css'],
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'bug/not-found').subscribe({
+      next: (resp) => {
+        console.log(resp);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'bug/bad-request').subscribe({
+      next: (resp) => {
+        console.log(resp);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'bug/server-error').subscribe({
+      next: (resp) => {
+        console.log(resp);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  get401Error() {
+    this.http.get(this.baseUrl + 'bug/auth').subscribe({
+      next: (resp) => {
+        console.log(resp);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  get400ValidationError() {
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe({
+      next: (resp) => {
+        console.log(resp);
+      },
+      error: (err) => {
+        console.log(err);
+        this.validationErrors = err;
+      },
+    });
+  }
+}
