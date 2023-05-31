94b22bd479a38d25cbeffb7c4c35c810745ecbc1
Matthew C <30327544+smile-eh@users.noreply.github.com>
2023-05-31 17:17:39 -0400

    add automapper profiles

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+using API.DTO;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(
+                    dest => dest.PhotoUrl,
+                    options => options.MapFrom(
+                        src => src.Photos.FirstOrDefault(x => x.IsMain).Url
+                    )
+                )
+                .ForMember(dest => dest.Age, options => options.MapFrom(
+                    src => src.DateOfBirth.CalculateAge()
+                ));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
