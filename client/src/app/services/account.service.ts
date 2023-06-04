ac0ead78e0fcd638324cfa8ab1b5606cc6266e91
Matthew C <30327544+smile-eh@users.noreply.github.com>
2023-06-04 17:26:07 -0400

    use env vars

--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -4,12 +4,13 @@ import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../models/user';
 import { Login } from '../models/login';
 import { Register } from '../models/register';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
