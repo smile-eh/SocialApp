f7cec5673096d4f557ee4ca558f8eaf001fc6a10
Matthew C <30327544+smile-eh@users.noreply.github.com>
2023-06-05 13:13:35 -0400

    add dto for updating user info

---
+++ API/DTO/MemberUpdateDto.cs
@@ -0,0 +1,12 @@
+namespace API.DTO
+{
+    public class MemberUpdateDto
+    {
+        public string Introduction { get; set; }
+        public string Bio { get; set; }
+        public string Interests { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+    }
+}
