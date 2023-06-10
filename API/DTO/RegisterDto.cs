4d9b5b9168bb5f5a4b987f4db024a7d521a0eab8
Matthew C <30327544+smile-eh@users.noreply.github.com>
2023-06-10 13:25:16 -0400

    update dtos for register forms

--- API/DTO/RegisterDto.cs
+++ API/DTO/RegisterDto.cs
@@ -11,5 +11,19 @@ namespace API.DTO
         [StringLength(128, MinimumLength = 4)]
         public string Password { get; set; }
 
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public DateOnly? DateOfBirth { get; set; } // optional to make required work, instead of default date
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
     }
 }
